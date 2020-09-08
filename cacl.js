d8d03fa64fe6cdd9880088acc23e2c157bee53dc
Κωνσταντίνος Μπατζιάκας <kmpatziakas@yahoo.gr>
2020-09-08 12:19:06 +0300

    Add files via upload

---
+++ cacl.js
@@ -0,0 +1,73 @@
+
+function insert(num){
+  input=document.getElementById('input');
+  switch(num){
+    case 1:
+      input.value += '1';
+      break;
+    case 2:
+      input.value += '2';
+      break;
+      case 3:
+        input.value += '3';
+        break;
+        case 4:
+          input.value += '4';
+          break;
+          case 5:
+            input.value += '5';
+            break;
+            case 6:
+              input.value += '6';
+              break;
+              case 7:
+                input.value += '7';
+                break;
+                case 8:
+                  input.value += '8';
+                  break;
+                  case 9:
+                    input.value += '9';
+                    break;
+                    case 0:
+                      input.value += '0';
+                      break;
+                      case ".":
+                        input.value +=".";
+                        break;
+  }
+}
+
+function getOperator(operator){
+  var input = document.getElementById('input');
+  switch (operator) {
+    case "+":
+      input.value +="+";
+      break;
+      case "-":
+        input.value +="-";
+        break;
+        case "*":
+          input.value +="*";
+          break;
+          case "/":
+            input.value +="/";
+            break;
+
+  }
+}
+
+function calculate(){
+  var input = document.getElementById('input');
+  var ans = eval(input.value);
+  document.getElementById('input').value = ans;
+  $("input").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#cal").click();
+    }
+});
+}
+
+function clearScreen(){
+  document.getElementById('input').value="";
+}
